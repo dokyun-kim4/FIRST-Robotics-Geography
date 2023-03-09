384d950d1d26697063ea47c3e3301fd9de9939e8
Dokyun Kim <donny5557@gmail.com>
2023-03-08 23:26:51 -0500

    added a function to isolate columns for map plot isolate_team_and_location()

--- DataFromFRC/b.py
+++ DataFromFRC/b.py
@@ -1,44 +1,8 @@
 """
 Code testing
 """
-import json
-import requests as rq
-import pandas as pd
-
-import data_functions as dfn
-from data_functions import TOKEN, HEADER
-
-CUTOFF = dfn.find_cutoff(dfn.read_text(dfn.build_url(2023, 1)))
-print(CUTOFF)
-
-test = []
-
-for i in range(1, 73):
-    print(i)
-    url = dfn.build_url(2023, i)
-    response = rq.request(
-        "GET",
-        url,
-        auth=TOKEN,
-        headers=HEADER,
-        data={},
-        timeout=10,
-    )
-    print(response.text)
-    print("\n---------------------------------\n")
-    # after_cut = dfn.trim_data(response.text)
-    # test += after_cut
-
-# print("Done")
-# df = pd.DataFrame(test)
-# df.to_csv("test.csv")
-
-# All = []
-# for i in range(1, 5):
-#     All += dfn.extract_data_one_page(2023, i)
-
-# df = pd.DataFrame(All)
-# df.to_csv("test.csv")
 
 
-# dfn.extract_data_all_pages(2019)
+import frc_data_functions as fdf
+
+print(fdf.isolate_team_and_location(fdf.extract_data_all_pages(2015)))
