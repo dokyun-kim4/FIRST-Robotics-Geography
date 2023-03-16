1bdc79d58cb4794334af20e8d779798d1d98cae5
Dokyun Kim <donny5557@gmail.com>
2023-03-16 19:54:24 -0400

    Created decode_png_one_year() For future: add try&catch for 'null' avatars

--- DataFromFRC/frc_avatar_functions.py
+++ DataFromFRC/frc_avatar_functions.py
@@ -4,6 +4,7 @@ Functions to obtain FRC team avatar & save it as .png files
 import base64
 import json
 import requests as rq
+import pandas as pd
 from apitoken import TOKEN
 
 HEADER = {
@@ -26,6 +27,7 @@ def build_url(year: int, team: int):
 
 
 def get_avatar_one_team(year: int, team: int):
+    #Will have to accomodate for teams w/ no avatar (returns 'null')
     """
     Gets the encoded png avatar of a given team in a given year
 
@@ -46,7 +48,7 @@ def get_avatar_one_team(year: int, team: int):
     return data["teams"][0]["encodedAvatar"]
 
 
-def decode_png(team: int, code: str):
+def decode_png(team: int, code: str, year: int):
     """
     Decodes base64 png code and saves a png file
 
@@ -54,7 +56,21 @@ def decode_png(team: int, code: str):
       team: An integer representing team name
       code: A string representing the encoded png
     """
-
-    with open(f"{team}.png", "wb") as pic:
+    
+    with open(f"{year}/{team}.png", "wb") as pic:
         code = bytes(code, "utf-8")
         pic.write(base64.decodebytes(code))
+
+
+def decode_png_one_year(year: int, dataframe: pd.DataFrame):
+    """
+    Saves ALL team avatars from one year
+
+    Args:
+        dataframe: A pandas dataframe that has team numbers
+    """
+    data = pd.read_csv(dataframe)
+    for _, row in data.iterrows():
+        current_team = int(row["teamNumber"])
+        current_team_code = get_avatar_one_team(year, current_team)
+        decode_png(current_team, current_team_code, year)
